02464574de4244b3f3cf2f6c5d264c14f71881fe
Monica Masae <monicamasae@Peters-MacBook-Air.local>
2022-09-16 14:03:23 +0300

    initial commit, add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "lilac": "#967bdc",
+        'blaze-orange': {
+          '50': '#fff8f2', 
+          '100': '#fff0e6', 
+          '200': '#ffdabf', 
+          '300': '#ffc499', 
+          '400': '#ff974d', 
+          '500': '#ff6b00', 
+          '600': '#e66000', 
+          '700': '#bf5000', 
+          '800': '#994000', 
+          '900': '#7d3400'
+      }
+      },
+    },
+  },
+  plugins: [],
+}
+

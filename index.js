85ac2e839508b384a6487c6a804beac3dd85f393
Monica Masae <monica.w.masae@gmail.com>
2022-09-27 15:27:54 +0300

    minor updates

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import jump from 'jump.js'
 const btn = document.getElementById("btn");
 const menu = document.querySelector(".menu");
 
@@ -5,3 +6,21 @@ btn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
 
 })
+
+jump('.pricing',{
+  duration: 1000,
+  offset: 0,
+  callback: undefined,
+  easing: easeInOutQuad,
+  a11y: false
+})
+
+
+const node = document.querySelector('.target')
+
+jump(node)
+
+// passing a CSS selector
+// the element referenced by the selector is determined using document.querySelector
+
+jump('.target')

81a9327e4e415ab07a60e8eb52194b0d268e99bb
M0nicah <monicawughanga@gmail.com>
2023-11-20 13:08:05 +0300

    (generated by blackbox.ai )

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require("tailwindcss/colors");
+// const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: [
@@ -9,19 +9,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        lilac: "#967bdc",
-        'blaze-orange': {
-          '50': '#fff8f2', 
-          '100': '#fff0e6', 
-          '200': '#ffdabf', 
-          '300': '#ffc499', 
-          '400': '#ff974d', 
-          '500': '#ff6b00', 
-          '600': '#e66000', 
-          '700': '#bf5000', 
-          '800': '#994000', 
-          '900': '#7d3400'
-      }
+        // lilac: "#967bdc",
+      //   
       },
     },
   },
